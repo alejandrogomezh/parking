dcf0d8f31880c2205c3ef66d4e31f09444e9ce8c
Alejandro Gómez Hurtado - Ceiba Software <alejandro.gomez@ceiba.com.co>
2018-02-26 15:01:52 -0500

    se quita @Sql("create_schema.sql") de ParkingApplication.java

--- src/main/java/co/ceiba/parking/ParkingApplication.java
+++ src/main/java/co/ceiba/parking/ParkingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootApplication
-@Sql("create_schema.sql")
 public class ParkingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApplication.class, args);
